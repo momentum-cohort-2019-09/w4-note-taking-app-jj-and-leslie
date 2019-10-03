8bc5afd966a4a95bb8c8571ffa44d382f8b20df7
lesliehealray <lesliehealray@gmail.com>
2019-10-03 16:52:19 -0400

    added login, basic auth, get notes

--- main.js
+++ main.js
@@ -9,8 +9,9 @@ let credentials = {
     password: sessionStorage.getItem('password')
 }
 
-function basicAuthCreds (username, password) {
-    return 'Basic' + btoa(`${username}:${password}`)
+function basicAuthCreds (credentials) {
+    console.log(credentials)
+    return 'Basic ' + btoa(`${credentials.username}:${credentials.password}`)
 }
 
 const loginForm =document.querySelector('#login-form') 
@@ -18,7 +19,6 @@ document.querySelector('#create-account').addEventListener('click', function(evt
     evt.preventDefault()
     let userName = document.getElementById('uname').value
     let passWord = document.getElementById('psw').value
-console.log(userName)
     fetch(`${baseApiUrl}/users`, {
         method: 'POST',
         body: JSON.stringify({'username': userName, 'password': passWord}),
@@ -29,10 +29,66 @@ console.log(userName)
 
     .then(response=> {
         if (response.ok){
+            credentials.username = username
+            credentials.password = password
+            sessionStorage.setItem('username', username)
+            sessionStorage.setItem('password', password)
+        } else {
             console.log(response.body)
+            document.getElementById('login-error').innerText = 'something went wrong'
         }
     })
-
 })
 
+function renderPage(){
+    if(!credentials.username || !credentials.password){
+        showLoginForm()
+    } else {
+        hideLoginForm()
+        getNotes()
+    }
+}
 
+function showLoginForm(){
+    document.getElementById('login-form').classList.remove('hidden')
+    document.getElementById('notes').classList.add('hidden')
+}
+
+function hideLoginForm () {
+    document.getElementById('login-form').classList.add('hidden')
+    document.getElementById('notes').classList.remove('hidden')
+}
+
+function renderNotes (notes) {
+    document.getElementById('notes').innerHTML = notes.map(note=>
+        `<div class="note">
+         <p>${note.title}</p>
+         <p>${note.text}</p>
+         <p>${note.tags}</p>
+         <p>${note.updated}</p>
+        </div>`
+    ).join('\n')
+}
+
+
+function getNotes() {
+    fetch(`${baseApiUrl}/notes`, {
+        method: 'GET',
+        headers: {
+            'Authorization': basicAuthCreds(credentials)
+        }
+    }) 
+    .then(response=> {
+        if (response.ok){
+            return response.json()
+        } else {
+            console.log(response);
+            document.getElementById('login-error').innerText = 'Invalid credentials'
+        }
+    })
+    .then(data=> {
+        renderNotes(data.notes)
+    })
+}
+
+renderPage()
